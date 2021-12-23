1f559c2028c2bf03cea6527944da74464ddae559
BenhadineKaoutar <kaoutarbenhadine@gmail.com>
2021-12-23 13:48:09 +0100

    Ajoutez des fichiers projet.

---
+++ venntest/ViewModels/AllProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using venntest.Models;
+
+namespace venntest.ViewModels
+{
+    public class AllProductsViewModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
